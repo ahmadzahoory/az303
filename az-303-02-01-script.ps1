27738807804fd10ed6ee6bb2d7ee460748cafa02
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-08-07 20:47:12 +0530

    Add files via upload

---
+++ az-303-02-01-script.ps1
@@ -0,0 +1,15 @@
+# Set execution policy
+Set-ExecutionPolicy Unrestricted -Force
+New-Item -ItemType directory -Path 'C:\temp'
+
+# Install IIS and Web Management Tools.
+Import-Module ServerManager
+install-windowsfeature web-server, web-webserver -IncludeAllSubFeature
+install-windowsfeature web-mgmt-tools
+
+#Change physical path of a site
+Import-Module WebAdministration
+Set-ItemProperty -path "IIS:\Sites\Default Web Site\" -name physicalPath -value "f:\webapp-code3"
+
+#Restart IIS
+iisreset /restart
