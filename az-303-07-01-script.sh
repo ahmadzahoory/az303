778f26d63a99035954be2f6495bccc0d6d5edb21
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-08-14 19:24:28 +0530

    Update az-303-07-01-script.sh

--- az-303-07-01-script.sh
+++ az-303-07-01-script.sh
@@ -4,10 +4,8 @@ apt-get install -y apache2
 apt-get install -y unzip
 apt-get install -y php7.2
 apt-get install -y php7.2-mysql
-apt-get install -y openssl shellinabox
 apt-get install -y mysql-server
 systemctl restart apache2.service
-/etc/init.d/shellinabox start
 cd /var/www/html/
 rm index.html
 wget https://raw.githubusercontent.com/ahmadzahoory/az303/master/az-303-07-01-code.zip
