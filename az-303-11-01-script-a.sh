f8c46ff8f0731bdff363c862162fc45bf48c83ba
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-08-16 21:24:50 +0530

    Update az-303-11-01-script-a.sh

--- az-303-11-01-script-a.sh
+++ az-303-11-01-script-a.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-sudo apt-get -y update
-sudo apt-get install -y apache2
-sudo apt-get install -y unzip
-sudo apt-get install -y php7.2
-sudo apt-get install -y php7.2-mysql
-sudo apt-get install -y mysql-server
-sudo systemctl restart apache2.service
+apt-get -y update
+apt-get install -y apache2
+apt-get install -y unzip
+apt-get install -y php7.2
+apt-get install -y php7.2-mysql
+apt-get install -y mysql-server
+systemctl restart apache2.service
 cd /var/www/html/
-sudo rm index.html
-sudo wget https://raw.githubusercontent.com/ahmadzahoory/az303/master/az-303-11-01-code.zip
-sudo unzip az-303-11-01-code.zip
+rm index.html
+wget https://raw.githubusercontent.com/ahmadzahoory/az303/master/az-303-11-01-code.zip
+unzip az-303-11-01-code.zip
